63ab2d58cc0c2a5047c9a899b0729510ae5ab504
Diego Ruiz <diego.ruiz@mapp.com>
2017-05-12 15:04:31 +0200

    Firsts snippets

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+
+public class FizzBuzz {
+
+    public String fizzBuzz(int number) {
+        StringBuilder sb = new StringBuilder();
+        if (number % 3 == 0)
+            sb.append("Fizz");
+        if (number % 5 == 0)
+            sb.append("Buzz");
+        if (sb.length()==0)
+            sb.append(number);
+        return sb.toString();
+    }
+
+    public String fizzBuzz8(int number) {
+        String result = Optional.of(number).map(n -> (n % 3 == 0 ? "Fizz" : "") + (n % 5 == 0 ? "Buzz" : "")).get();
+        return result.isEmpty() ? Integer.toString(number) : result;
+    }
+
+}
---
+++ src/main/java/IntegerUtils.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+
+/**
+ * Created by dr186049 on 5/12/2017.
+ */
+public class IntegerUtils {
+
+    public static void main(String[] args) {
+        int riskyInt = Integer.MAX_VALUE-1;
+        System.out.println("Checking int limit (" + Integer.MAX_VALUE + "): " + (riskyInt+2));
+
+        Integer riskyInteger = Integer.MAX_VALUE-1;
+        System.out.println("Checking Integer limit (" + Integer.MAX_VALUE + "): " + Integer.sum(riskyInteger,2));
+
+        System.out.println("Checking Integer limit 1 (" + Integer.MAX_VALUE + "): " + checkedAdd(riskyInteger,1));
+
+        try {
+            System.out.println("Checking Integer limit 2 (" + Integer.MAX_VALUE + "): " + checkedAdd(riskyInteger,2));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        BigInteger bigInteger = new BigInteger(String.valueOf(Integer.MAX_VALUE));
+        System.out.println("Checking BigInteger limit: " + bigInteger.add(new BigInteger("5465465465465422112")));
+    }
+
+    protected static int checkedAdd(int a, int b) {
+        boolean overflow = a > Integer.MAX_VALUE - b;
+        long upperType = a + b; // bitwise check
+        if(upperType >>> 32 != 0) {
+            throw new ArithmeticException("Overflow summing " + a + " + " + b);
+        } else {
+            return a+b;
+        }
+    }
+}
